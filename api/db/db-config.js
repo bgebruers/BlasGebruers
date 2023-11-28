ac1f75f4a2672f44c89862185506eaa6a5b10e53
bgebruers <132944653+bgebruers@users.noreply.github.com>
2023-11-28 17:30:52 -0300

    Delete api directory

--- api/db/db-config.js
+++
@@ -1,7 +0,0 @@
-//se usa para que desde otro lado se pueda encontrar
-module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "bananatv"
-};
--- api/model/tarjeta-model.js
+++
@@ -1,9 +0,0 @@
-// constructor
-const Tarjeta = function (tarjeta) {
-  this.nroTarjeta = tarjeta.nroTarjeta;
-  this.vencimiento = tarjeta.vencimiento;
-  this.ccv= tarjeta.ccv;
-  this.idUsuario = tarjeta.idUsuario;
-};
-
-module.exports = Tarjeta;
